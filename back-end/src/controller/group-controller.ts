517424c44f9f64df3bde31845c5baa5958b96a02
Sujit M <s.mahavarkar@gmail.com>
2021-06-01 16:16:33 +0700

    adds create group API

--- back-end/src/controller/group-controller.ts
+++ back-end/src/controller/group-controller.ts
@@ -1,45 +1,54 @@
 import { NextFunction, Request, Response } from "express"
+import { getRepository } from "typeorm"
+import { Group } from "../entity/group.entity"
+import { CreateGroupInput } from "../interface/group.interface"
 
 export class GroupController {
-
+  private groupRepository = getRepository(Group)
   async allGroups(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
+    // Task 1:
     // Return the list of all groups
   }
 
   async createGroup(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
+    // Task 1:
+
     // Add a Group
+    const { body: params } = request
+    console.log(params)
+    const createGroupInput: CreateGroupInput = {
+      name: params.name,
+      number_of_weeks: params.number_of_weeks,
+      roll_states: params.roll_states,
+      incidents: params.incidents,
+      ltmt: params.ltmt,
+      run_at: params.run_at,
+      student_count: params.student_count,
+    }
+    const group = new Group()
+    group.prepareToCreate(createGroupInput)
+    return this.groupRepository.save(group)
   }
 
   async updateGroup(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
+    // Task 1:
     // Update a Group
   }
 
   async removeGroup(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
+    // Task 1:
     // Delete a Group
   }
 
   async getGroupStudents(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-        
+    // Task 1:
     // Return the list of Students that are in a Group
   }
 
-
   async runGroupFilters(request: Request, response: Response, next: NextFunction) {
     // Task 2:
-  
     // 1. Clear out the groups (delete all the students from the groups)
-
     // 2. For each group, query the student rolls to see which students match the filter for the group
-
     // 3. Add the list of students that match the filter to the group
   }
 }
--- back-end/src/entity/group.entity.ts
+++ back-end/src/entity/group.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { CreateGroupInput } from "../interface/group.interface"
 
 @Entity()
 export class Group {
@@ -28,4 +29,14 @@ export class Group {
   @Column()
   student_count: number
 
+  public prepareToCreate(input: CreateGroupInput) {
+    this.name = input.name
+    this.number_of_weeks = input.number_of_weeks
+    // does not throw error on sending other than mentioned strings -> only not null error
+    if (input.roll_states === "unmark" || input.roll_states === "present" || input.roll_states === "absent" || input.roll_states === "late") this.roll_states = input.roll_states
+    this.incidents = input.incidents
+    // does not throw error on sending other than mentioned strings -> only not null error
+    if (input.ltmt === "<" || input.ltmt === ">") this.ltmt = input.ltmt
+    this.student_count = input.student_count
+  }
 }
